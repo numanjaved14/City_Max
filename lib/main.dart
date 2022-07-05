c3f3f153e37b14550364f3108a939010948c3f10
fwdAndroid <fwdkaleem@gmail.com>
2022-07-05 19:34:32 +0500

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:city_max/splash/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+       MyApp(),
+    
+  );
 }
 
 class MyApp extends StatelessWidget {
