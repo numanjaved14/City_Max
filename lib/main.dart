76b7258024140d39e351dbd0a6e4ce2a351bdde4
fwdAndroid <fwdkaleem@gmail.com>
2022-07-07 02:14:31 +0500

    AuthState Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:city_max/main/mainscreen.dart';
 import 'package:city_max/splash/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'onboardingscreens/welcome.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +21,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -31,7 +36,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Splash()
+      home:  FirebaseAuth.instance.currentUser == null
+          ?  Welcome()
+          : MainScreen(),
     );
   }
 }
