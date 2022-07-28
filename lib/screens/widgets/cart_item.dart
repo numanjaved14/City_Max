3f0a9d8926988872e685a3dee627e2abeab898d9
fwdAndroid <fwdkaleem@gmail.com>
2022-07-28 13:29:16 +0500

    Last Change

--- lib/screens/widgets/cart_item.dart
+++ lib/screens/widgets/cart_item.dart
@@ -20,48 +20,40 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
-      key: ValueKey(id),
-      background: Container(
-        color: Theme.of(context).errorColor,
-        child: Icon(
-          Icons.delete,
-          color: Colors.white,
-          size: 40,
-        ),
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.only(right: 20),
-        margin: EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 4,
-        ),
-      ),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction) {
-        Provider.of<Cart>(context, listen: false).removeItem(uuid: uuid);
-      },
-      child: Card(
-        margin: EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 4,
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Padding(
-                padding: EdgeInsets.all(5),
-                child: FittedBox(
-                  child: Text('\$$price'),
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      child: Column(children: [
+        Container(
+          child: Card(
+            margin: EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 4,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: FittedBox(
+                      child: Text('\$$price'),
+                    ),
+                  ),
                 ),
+                title: Text(title),
+                subtitle: Text('Total: \$${(price * quantity)}'),
+                trailing: TextButton(
+                    onPressed: () {
+                      Provider.of<Cart>(context, listen: false)
+                          .removeItem(uuid: uuid);
+                    },
+                    child: Text('Delete')),
+                // trailing: Text('$quantity x'),
               ),
             ),
-            title: Text(title),
-            subtitle: Text('Total: \$${(price * quantity)}'),
-            trailing: Text('$quantity x'),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
