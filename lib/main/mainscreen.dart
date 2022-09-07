c092c2ed14a7b32330e34595e724544bb3159ac4
fwdAndroid <fwdkaleem@gmail.com>
2022-09-07 19:14:07 +0500

    Bottom Changed

--- lib/main/mainscreen.dart
+++ lib/main/mainscreen.dart
@@ -1,9 +1,10 @@
+import 'dart:ui';
+
 import 'package:city_max/bottom_pages/booking.dart';
 import 'package:city_max/bottom_pages/my_homepage.dart';
 import 'package:city_max/bottom_pages/noti.dart';
 import 'package:city_max/bottom_pages/user.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -12,8 +13,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   // Properties & Variables needed
-  PersistentTabController _controller =
-      PersistentTabController(initialIndex: 0);
+
   int currentTab = 0; // to keep track of active tab index
   final List<Widget> screens = [
     MyHomePage(),
@@ -27,77 +27,108 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PersistentTabView(
-        screens: screens,
-        backgroundColor: Colors.white, // Default is Colors.white.
-        handleAndroidBackButtonPress: true, // Default is true.
-        resizeToAvoidBottomInset:
-            true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-        stateManagement: true, // Default is true.
-        hideNavigationBarWhenKeyboardShows:
-            true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-        decoration: NavBarDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          colorBehindNavBar: Colors.white,
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        MyHomePage(); // if user taps on this dashboard tab will be active
+                    currentTab = 0;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/home.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Home")
+                  ],
+                ),
+              ),
+              //Booking
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen = Booking();
+                    // if user taps on this dashboard tab will be active
+                    currentTab = 1;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/deadline.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Booking")
+                  ],
+                ),
+              ),
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        Notifications(); // if user taps on this dashboard tab will be active
+                    currentTab = 2;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/notification.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Notification")
+                  ],
+                ),
+              ),
+
+              ///Create Orders
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        User(); // if user taps on this dashboard tab will be active
+                    currentTab = 4;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/man.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Profile")
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-        popAllScreensOnTapOfSelectedTab: true,
-        popActionScreens: PopActionScreensType.all,
-        itemAnimationProperties: ItemAnimationProperties(
-          // Navigation Bar's items animation properties.
-          duration: Duration(milliseconds: 200),
-          curve: Curves.ease,
-        ),
-        screenTransitionAnimation: ScreenTransitionAnimation(
-          // Screen transition animation on change of selected tab.
-          animateTabTransition: true,
-          curve: Curves.ease,
-          duration: Duration(milliseconds: 200),
-        ),
-        context,
-        controller: _controller,
-        // items: [],
-        items: [
-          PersistentBottomNavBarItem(
-            icon: Image.asset(
-              'assets/home.png',
-              height: 30,
-              width: 30,
-            ),
-            title: ("Home"),
-            activeColorPrimary: Color(0xff0DC6DF),
-            inactiveColorPrimary: Color(0xff8d8282),
-          ),
-          PersistentBottomNavBarItem(
-            icon: Image.asset(
-              'assets/deadline.png',
-              height: 30,
-              width: 30,
-            ),
-            title: ("Booking"),
-            activeColorPrimary: Color(0xff0DC6DF),
-            inactiveColorPrimary: Color(0xff8d8282),
-          ),
-          PersistentBottomNavBarItem(
-            icon: Image.asset(
-              'assets/notification.png',
-              height: 30,
-              width: 30,
-            ),
-            title: ("Notifications"),
-            activeColorPrimary: Color(0xff0DC6DF),
-            inactiveColorPrimary: Color(0xff8d8282),
-          ),
-          PersistentBottomNavBarItem(
-            icon: Image.asset(
-              'assets/man.png',
-              height: 30,
-              width: 30,
-            ),
-            title: ("Profile"),
-            activeColorPrimary: Color(0xff0DC6DF),
-            inactiveColorPrimary: Color(0xff8d8282),
-          ),
-        ],
       ),
     );
   }
